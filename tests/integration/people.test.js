80508d3bc8243c0d714f4d779d91082df7294295
Beatriz <beatriz.mediatto2@gmail.com>
2023-02-08 10:09:07 -0300

    adicionando testes

--- tests/integration/people.test.js
+++ tests/integration/people.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
+
 const app = require('../../src/app');
 const connection = require('../../src/db/connection');
 
@@ -8,22 +9,41 @@ const { expect, use } = chai;
 
 use(chaiHttp);
 
-describe('Testando os endpoint de people', () => {
-  it('Testando o cadastro de uma pessoa', async () => {
+const peopleList = [
+  {
+    id: 1,
+    firstName: 'Luke',
+    lastName: 'Skywalker',
+    email: 'luke.skywalker@trybe.com',
+    phone: '851 678 4453',
+  },
+  {
+    id: 2,
+    firstName: 'Dart',
+    lastName: 'Vader',
+    email: 'dart.vader@trybe.com',
+    phone: '851 678 5665',
+  },
+];
+
+describe('Testando os endpoints de people', () => {
+  it('Testando o cadastro de uma pessoa ', async () => {
     sinon.stub(connection, 'execute').resolves([{ insertId: 42 }]);
 
-    const response = await (await chai.request(app).post('/people')).setEncoding(
-      {
-      firstName: 'Luke',
-      lasName: 'Skywalker',
-      email: 'luke.skywalker@trybe.com',
-      phone: '851 678 4453',
-    },
-    );
-    
+    const response = await chai.request(app).post('/people').send(
+        {
+          firstName: 'Luke',
+          lastName: 'Skywalker',
+          email: 'luke.skywalker@trybe.com',
+          phone: '851 678 4453',
+        },
+      );
+
     expect(response.status).to.equal(201);
-    expect(response.body).to.deep.equal({ message: 'Pessoa cadastrada com sucesso com o id 42' });
+    expect(response.body).to
+      .deep.equal({ message: 'Pessoa cadastrada com sucesso com o id 42' });
   });
+  it();
 
   afterEach(sinon.restore);
 });
